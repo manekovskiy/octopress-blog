1bb36d4d43a632ca952a94d3080f5af3b0fdb53b
Alex Manekovskyi <873602+manekovskiy@users.noreply.github.com>
2013-03-30 16:53:17 +0200

    added first try on configurations

--- config.rb
+++ config.rb
@@ -2,10 +2,10 @@
 project_type = :stand_alone
 
 # Publishing paths
-http_path = "/"
-http_images_path = "/images"
-http_fonts_path = "/fonts"
-css_dir = "public/stylesheets"
+http_path = "/octopress-blog/"
+http_images_path = "/octopress-blog/images"
+http_fonts_path = "/octopress-blog/fonts"
+css_dir = "public/octopress-blog/stylesheets"
 
 # Local development paths
 sass_dir = "sass"
@@ -14,3 +14,4 @@ fonts_dir = "source/fonts"
 
 line_comments = false
 output_style = :compressed
+
